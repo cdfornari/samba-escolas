5850b94e454626d76ffcd5a0a0d1d634387100b6
Carlos Fornari <lilfornah@gmail.com>
2023-06-07 23:25:14 -0400

    feat(api): :label: escolas dtos

--- apps/api/src/escolas/dto/create-escola.input.ts
+++ apps/api/src/escolas/dto/create-escola.input.ts
@@ -1,7 +1,52 @@
 import { InputType, Int, Field } from '@nestjs/graphql';
+import {
+  IsBoolean,
+  IsDate,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 
 @InputType()
 export class CreateEscolaInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @IsString()
+  @IsNotEmpty()
+  @Field(() => String)
+  nombre: string;
+
+  @IsString()
+  @IsOptional()
+  @Field(() => String, { nullable: true })
+  descripcion?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Field(() => String)
+  direccion_sede: string;
+
+  @IsNumber()
+  @Field(() => Int)
+  numero: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @Field(() => String)
+  cep: string;
+
+  @IsDate()
+  @Field(() => Date)
+  fecha_fundacion: Date;
+
+  @IsString()
+  @IsNotEmpty()
+  @Field(() => String)
+  resumen_historico: string;
+
+  @IsBoolean()
+  @IsOptional()
+  @Field(() => Boolean, { nullable: true })
+  gres: boolean;
+
+  //TODO: CIUDAD
 }
--- apps/api/src/escolas/dto/update-escola.input.ts
+++ apps/api/src/escolas/dto/update-escola.input.ts
@@ -1,8 +1,11 @@
+import { IsInt, IsPositive } from 'class-validator';
 import { CreateEscolaInput } from './create-escola.input';
 import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
 
 @InputType()
 export class UpdateEscolaInput extends PartialType(CreateEscolaInput) {
+  @IsInt()
+  @IsPositive()
   @Field(() => Int)
   id: number;
 }
