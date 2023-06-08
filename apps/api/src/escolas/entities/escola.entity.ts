67c792f12f079cab00ad6174df4bd9ec204491d0
Carlos Fornari <lilfornah@gmail.com>
2023-06-07 23:23:13 -0400

    feat(api): :label: escola object type

--- apps/api/src/escolas/entities/escola.entity.ts
+++ apps/api/src/escolas/entities/escola.entity.ts
@@ -2,6 +2,32 @@ import { ObjectType, Field, Int } from '@nestjs/graphql';
 
 @ObjectType()
 export class Escola {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => Int)
+  id: number;
+
+  @Field(() => String)
+  nombre: string;
+
+  @Field(() => String, { nullable: true })
+  descripcion?: string;
+
+  @Field(() => String)
+  direccion_sede: string;
+
+  @Field(() => Int)
+  numero: number;
+
+  @Field(() => String)
+  cep: string;
+
+  @Field(() => Date)
+  fecha_fundacion: Date;
+
+  @Field(() => String)
+  resumen_historico: string;
+
+  @Field(() => Boolean, { nullable: true })
+  gres: boolean;
+
+  //TODO: CIUDAD 
 }
