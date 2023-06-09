35a48441bff43d28643157211880e8b14f3d4b0a
Carlos Fornari <lilfornah@gmail.com>
2023-06-08 23:12:44 -0400

    fix(database): :bug: fix typo

--- database/database.sql
+++ database/database.sql
@@ -49,7 +49,7 @@ CREATE TABLE IF NOT EXISTS escuelas_samba(
   fecha_fundacion DATE NOT NULL,
   resumen_historico VARCHAR(500) NOT NULL,
   id_ciudad INTEGER NOT NULL REFERENCES lugares_geo(id),
-  gres BOOLEAN;
+  gres BOOLEAN,
   descripcion VARCHAR(500)
 );
 
