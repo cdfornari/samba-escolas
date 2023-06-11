e4c5e432ea3915749237ca9ad0905f86b787d0e7
Carlos Fornari <lilfornah@gmail.com>
2023-06-11 12:39:42 -0400

    chore(database): :seedling: update seed

--- database/seed.sql
+++ database/seed.sql
@@ -1,6 +1,13 @@
 BEGIN;
+INSERT INTO lugares_geo (nombre, tipo) VALUES ('Sudeste', 'region');
+COMMIT;
 
-INSERT INTO habilidades (nombre, descripcion) VALUES
-    ('test', 'test');
+BEGIN;
+INSERT INTO lugares_geo (nombre, tipo, id_padre_lugar) VALUES ('Rio de Janeiro', 'estado', 1);
+COMMIT;
 
+BEGIN;
+INSERT INTO lugares_geo (nombre, tipo, id_padre_lugar) VALUES ('Rio de Janeiro', 'ciudad', 2);
+INSERT INTO escuelas_samba (nombre, dir_sede, numero, cep, fecha_fundacion, resumen_historico, id_ciudad, gres, descripcion)
+VALUES ('Imperatriz Leopoldinense', ' R. Professor Lacé', 235, '59155190', '03-06-1956', 'Fundada el 6 de marzo de 1959 por el farmacéutico Amaury Jório, junto con algunos sambistas de la Zona Leopoldina y remanentes de la extinta asociación Recreio de Ramos, su nombre hace referencia al Ferrocarril Leopoldina -que atravesaba el barrio de Ramos- y que, a su vez, , recibió este nombre en referencia a la emperatriz María Leopoldina de Brasil', 3, TRUE, 'La mejor escola de samba de Rio de Janeiro');
 COMMIT;
