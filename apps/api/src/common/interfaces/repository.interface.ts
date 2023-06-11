e61b7275bb45a790c5c362d3069ac90995cb1e25
Carlos Fornari <lilfornah@gmail.com>
2023-06-11 12:46:53 -0400

    chore(api): :fire:

--- apps/api/src/common/interfaces/repository.interface.ts
+++
@@ -1,7 +0,0 @@
-import { PaginationArgs } from '../dto/args/pagination.args';
-
-export interface Repository<T, I> {
-  findMany: (pagination?: PaginationArgs) => Promise<T[]>;
-  findOne: (id: I) => Promise<T>;
-  save: (entity: T) => Promise<T>;
-}
