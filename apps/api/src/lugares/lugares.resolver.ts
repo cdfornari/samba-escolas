f84a3e29f19851dc11d4dc19a8d00a4e7ec5ebfe
Carlos Fornari <lilfornah@gmail.com>
2023-06-11 14:03:20 -0400

    fix(api): :pencil2: fix typo

--- apps/api/src/lugares/lugares.resolver.ts
+++ apps/api/src/lugares/lugares.resolver.ts
@@ -1,34 +1,34 @@
 import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
 import { LugaresService } from './lugares.service';
-import { Lugare } from './entities/lugare.entity';
+import { Lugar } from './entities/lugar.entity';
 import { CreateLugareInput } from './dto/create-lugare.input';
 import { UpdateLugareInput } from './dto/update-lugare.input';
 
-@Resolver(() => Lugare)
+@Resolver(() => Lugar)
 export class LugaresResolver {
   constructor(private readonly lugaresService: LugaresService) {}
 
-  @Mutation(() => Lugare)
+  @Mutation(() => Lugar)
   createLugare(@Args('createLugareInput') createLugareInput: CreateLugareInput) {
     return this.lugaresService.create(createLugareInput);
   }
 
-  @Query(() => [Lugare], { name: 'lugares' })
+  @Query(() => [Lugar], { name: 'lugares' })
   findAll() {
     return this.lugaresService.findAll();
   }
 
-  @Query(() => Lugare, { name: 'lugare' })
+  @Query(() => Lugar, { name: 'lugare' })
   findOne(@Args('id', { type: () => Int }) id: number) {
     return this.lugaresService.findOne(id);
   }
 
-  @Mutation(() => Lugare)
+  @Mutation(() => Lugar)
   updateLugare(@Args('updateLugareInput') updateLugareInput: UpdateLugareInput) {
     return this.lugaresService.update(updateLugareInput.id, updateLugareInput);
   }
 
-  @Mutation(() => Lugare)
+  @Mutation(() => Lugar)
   removeLugare(@Args('id', { type: () => Int }) id: number) {
     return this.lugaresService.remove(id);
   }
