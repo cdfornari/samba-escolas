8d4fda0e102a8b4259847afb3cfab8711ce5d234
Carlos Fornari <lilfornah@gmail.com>
2023-06-12 22:10:55 -0400

    feat(api): :sparkles: get parent place

--- apps/api/src/lugares/lugares.resolver.ts
+++ apps/api/src/lugares/lugares.resolver.ts
@@ -1,4 +1,12 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Args,
+  Int,
+  ResolveField,
+  Parent,
+} from '@nestjs/graphql';
 import { LugaresService } from './lugares.service';
 import { Lugar } from './entities/lugar.entity';
 import { CreateLugareInput } from './dto/create-lugare.input';
@@ -12,14 +20,14 @@ export class LugaresResolver {
   constructor(private readonly lugaresService: LugaresService) {}
 
   @Mutation(() => Lugar)
-  createLugare(@Args('createLugareInput') createLugareInput: CreateLugareInput) {
+  createLugare(
+    @Args('createLugareInput') createLugareInput: CreateLugareInput,
+  ) {
     return this.lugaresService.create(createLugareInput);
   }
 
   @Query(() => LugaresPaginationType, { name: 'lugares' })
-  async findAll(
-    @Args() pagination: PaginationArgs,
-  ) {
+  async findAll(@Args() pagination: PaginationArgs) {
     const [items, count] = await Promise.all([
       this.lugaresService.findAll(pagination),
       this.lugaresService.count(),
@@ -36,12 +44,21 @@ export class LugaresResolver {
   }
 
   @Mutation(() => Lugar)
-  updateLugare(@Args('updateLugareInput') updateLugareInput: UpdateLugareInput) {
+  updateLugare(
+    @Args('updateLugareInput') updateLugareInput: UpdateLugareInput,
+  ) {
     return this.lugaresService.update(updateLugareInput.id, updateLugareInput);
   }
 
   @Mutation(() => Lugar)
   removeLugare(@Args('id', { type: () => Int }) id: number) {
     return this.lugaresService.remove(id);
+  }
+
+  @ResolveField(() => Lugar, { name: 'padre', nullable: true })
+  async getParent(@Parent() lugar: Lugar) {
+    return lugar.id_padre_lugar
+      ? this.lugaresService.findOne(lugar.id_padre_lugar)
+      : null;
   }
 }
