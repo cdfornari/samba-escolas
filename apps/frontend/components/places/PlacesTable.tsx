2fe5b320ae0d7133bf1bdcf4228dce24a0f6cb4d
Carlos Fornari <lilfornah@gmail.com>
2023-06-15 12:40:47 -0400

    feat(frontend): :children_crossing: improve navigation

--- apps/frontend/components/places/PlacesTable.tsx
+++ apps/frontend/components/places/PlacesTable.tsx
@@ -1,11 +1,12 @@
 'use client';
 import { useState } from 'react';
 import { useQuery } from '@apollo/client';
-import { Loading, SortDescriptor, Table } from '@nextui-org/react';
+import { Button, Loading, SortDescriptor, Table } from '@nextui-org/react';
 import { LUGARES } from '../../graphql';
 import { PaginationType } from '../../types';
 import { Place } from '../../interfaces';
 import { Pagination } from '../ui/Pagination';
+import { useRouter } from 'next/navigation';
 
 const columns = [
   {
@@ -27,6 +28,7 @@ const columns = [
 ];
 
 export const PlacesTable = () => {
+  const {push} = useRouter()
   const [page, setPage] = useState(1);
   const [sortDescriptor, setSortDescriptor] = useState<SortDescriptor>();
   const { data, loading, error } = useQuery<{
@@ -47,55 +49,65 @@ export const PlacesTable = () => {
     );
   if (error) return <p>Error</p>;
   return (
-    <>
-      <Table
-        bordered
-        selectionMode="single"
-        onSelectionChange={(row) => console.log(row)}
-        onSortChange={(descriptor: SortDescriptor) =>
-          setSortDescriptor(descriptor)
-        }
-        sortDescriptor={sortDescriptor}
-      >
-        <Table.Header>
-          {columns.map((column) => (
-            <Table.Column
-              key={column.key}
-              allowsSorting
-              css={{
-                '& > svg': {
-                  right: 10,
-                },
-              }}
-            >
-              {column.label}
-            </Table.Column>
-          ))}
-        </Table.Header>
-        <Table.Body
-          items={data.lugares.items.map((lugar) => ({
-            ...lugar,
-            padre: lugar.padre?.nombre ?? 'Brasil',
-          }))}
+    <div className="flex h-full flex-col justify-between">
+      <div className="flex justify-end py-6 px-10">
+        <Button
+          auto
+          onClick={() => push('/places/create')}
         >
-          {(row) => (
-            <Table.Row key={row.id}>
-              {(columnKey) => (
-                <Table.Cell css={{ cursor: 'pointer' }}>
-                  {row[columnKey]}
-                </Table.Cell>
-              )}
-            </Table.Row>
-          )}
-        </Table.Body>
-      </Table>
-      <Pagination
-        page={page}
-        perPage={page === 1 ? data?.lugares.items.length : 5}
-        setPage={setPage}
-        totalPages={data?.lugares.numberOfPages ?? 0}
-        totalItems={data?.lugaresCount ?? 0}
-      />
-    </>
+          Crear Lugar
+        </Button>
+      </div>
+      <div className="flex h-full flex-col justify-between">
+        <Table
+          bordered
+          selectionMode="single"
+          onSelectionChange={(row) => console.log(row)}
+          onSortChange={(descriptor: SortDescriptor) =>
+            setSortDescriptor(descriptor)
+          }
+          sortDescriptor={sortDescriptor}
+        >
+          <Table.Header>
+            {columns.map((column) => (
+              <Table.Column
+                key={column.key}
+                allowsSorting
+                css={{
+                  '& > svg': {
+                    right: 10,
+                  },
+                }}
+              >
+                {column.label}
+              </Table.Column>
+            ))}
+          </Table.Header>
+          <Table.Body
+            items={data.lugares.items.map((lugar) => ({
+              ...lugar,
+              padre: lugar.padre?.nombre ?? 'Brasil',
+            }))}
+          >
+            {(row) => (
+              <Table.Row key={row.id}>
+                {(columnKey) => (
+                  <Table.Cell css={{ cursor: 'pointer' }}>
+                    {row[columnKey]}
+                  </Table.Cell>
+                )}
+              </Table.Row>
+            )}
+          </Table.Body>
+        </Table>
+        <Pagination
+          page={page}
+          perPage={page === 1 ? data?.lugares.items.length : 5}
+          setPage={setPage}
+          totalPages={data?.lugares.numberOfPages ?? 0}
+          totalItems={data?.lugaresCount ?? 0}
+        />
+      </div>
+    </div>
   );
 };
