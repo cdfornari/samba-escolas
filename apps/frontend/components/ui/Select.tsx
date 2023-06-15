01a3e81f1c0e3852b249bc169be23e56d9b1ccb8
Carlos Fornari <lilfornah@gmail.com>
2023-06-15 11:03:43 -0400

    fix(frontend): :bug: fix void select button

--- apps/frontend/components/ui/Select.tsx
+++ apps/frontend/components/ui/Select.tsx
@@ -18,9 +18,9 @@ export const Select: FC<Props> = ({ selected, setSelected, options, label }) => 
           <Listbox.Label className="block text-sm font-medium leading-6 text-gray-900">
             {label}
           </Listbox.Label>
-          <div className="relative mt-2">
-            <Listbox.Button className="relative w-full cursor-pointer rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
-              <span className="flex items-center">
+          <div className="relative">
+            <Listbox.Button className="relative w-full h-10 cursor-pointer rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
+              <span className="flex items-center min-h-50">
                 <span className="ml-3 block truncate">{selected}</span>
               </span>
               <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
