7578e245e4cb350804a0c0a142350456c6c252f1
Carlos Fornari <lilfornah@gmail.com>
2023-06-23 18:10:14 -0400

    feat(api): :sparkles: CRUD service

--- apps/api/src/common/common.module.ts
+++ apps/api/src/common/common.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { QueryService } from './services/query.service';
 import { DateScalar } from './scalars/date.scalar';
+import { CRUDService } from './services/crud.service';
 
 @Module({
-  providers: [QueryService, DateScalar],
-  exports: [QueryService],
+  providers: [CRUDService, QueryService, DateScalar],
+  exports: [CRUDService, QueryService],
 })
 export class CommonModule {}
---
+++ apps/api/src/common/services/crud.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@nestjs/common';
+import { QueryService } from './query.service';
+import { PaginationArgs } from '../dto/args/pagination.args';
+
+@Injectable()
+export class CRUDService {
+  constructor(private readonly queryService: QueryService) {}
+
+  async create<T, I>(tableName, input: I): Promise<T> {
+    let fields = Object.keys(input);
+    let values = Object.values(input);
+    fields = fields.filter((field) => !!input[field]);
+    values = values.filter((value) => !!value);
+    return (await this.queryService.insert<T>(tableName, fields, values))[0];
+  }
+
+  async findAll<T, F extends { [key: string]: any; type: 'OR' | 'AND' }>(
+    tableName: string,
+    pagination: PaginationArgs,
+    filter?: F,
+  ): Promise<T[]> {
+    return this.queryService.select<T[]>(
+      tableName,
+      null,
+      filter
+        ? `
+        ${Object.keys(filter)
+          .map((key) => `${key} = '${filter[key]}'`)
+          .join(` ${filter.type} `)}
+      `
+        : null,
+      null,
+      pagination,
+    );
+  }
+
+  async findOne(tableName: string, id: number) {
+    return (await this.queryService.select(tableName, null, `id = ${id}`))[0];
+  }
+
+  async updateOne<T, I extends { [key: string]: any }>(
+    tableName: string,
+    id: string,
+    input: I,
+  ): Promise<T> {
+    return (
+      await this.queryService.update(tableName, input, `id = ${id}`)
+    )[0][0];
+  }
+
+  remove(tableName: string, id: number) {
+    return `This action removes a #${id} entity`;
+  }
+}
