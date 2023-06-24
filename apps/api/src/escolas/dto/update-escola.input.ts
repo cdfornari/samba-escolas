e27e7a941c32770f836e8f7115cdb902572b902b
Sspa1 <sandrospaa@gmail.com>
2023-06-24 02:27:20 -0400

    feat: arreglado updateDto

--- apps/api/src/escolas/dto/update-escola.input.ts
+++ apps/api/src/escolas/dto/update-escola.input.ts
@@ -4,6 +4,7 @@ import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
 
 @InputType()
 export class UpdateEscolaInput extends PartialType(CreateEscolaInput) {
+  
   @IsInt()
   @IsPositive()
   @Field(() => Int)
--- apps/api/src/premios/dto/update-premio.input.ts
+++ apps/api/src/premios/dto/update-premio.input.ts
@@ -1,8 +1,12 @@
+import { IsInt, IsPositive } from 'class-validator';
 import { CreatePremioInput } from './create-premio.input';
 import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
 
 @InputType()
 export class UpdatePremioInput extends PartialType(CreatePremioInput) {
+  
+  @IsInt()
+  @IsPositive()
   @Field(() => Int)
   id: number;
 }
