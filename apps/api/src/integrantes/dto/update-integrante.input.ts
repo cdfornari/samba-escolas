84651ccc1236f7d77e2e3ec4f37fb26413934c12
Carlos Fornari <lilfornah@gmail.com>
2023-06-24 01:02:20 -0400

    fix(api): :bug: add id to update input

--- apps/api/src/integrantes/dto/update-integrante.input.ts
+++ apps/api/src/integrantes/dto/update-integrante.input.ts
@@ -1,8 +1,11 @@
+import { IsInt, IsPositive } from 'class-validator';
 import { CreateIntegranteInput } from './create-integrante.input';
 import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
 
 @InputType()
 export class UpdateIntegranteInput extends PartialType(CreateIntegranteInput) {
+  @IsInt()
+  @IsPositive()
   @Field(() => Int)
   id: number;
 }
