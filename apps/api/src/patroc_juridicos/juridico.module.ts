885db1438d8b96201488b4007ecd0aff00a3521a
Holeekey <danbrunosv@gmail.com>
2023-06-25 17:39:46 -0400

    patroc juridicos fix #2

--- apps/api/src/patroc_juridicos/juridico.module.ts
+++ apps/api/src/patroc_juridicos/juridico.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CommonModule } from 'src/common/common.module';
 import { JuridicosService } from './juridico.service';
 import { JuridicosResolver } from './juridico.resolver';
+import { LugaresModule } from 'src/lugares/lugares.module';
 
 @Module({
-  imports: [CommonModule],
+  imports: [CommonModule,LugaresModule],
   providers: [JuridicosResolver, JuridicosService],
 })
 export class JuridicosModule {
