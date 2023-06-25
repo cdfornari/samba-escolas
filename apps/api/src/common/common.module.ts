0a771935bc4e9afb65e94c89d332383136cda8c2
Sspa1 <sandrospaa@gmail.com>
2023-06-25 15:45:42 -0400

    Remove TelfService from the common module

--- apps/api/src/common/common.module.ts
+++ apps/api/src/common/common.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { QueryService } from './services/query.service';
 import { DateScalar } from './scalars/date.scalar';
 import { CRUDService } from './services/crud.service';
-import { TelfService } from './services/telf.service';
 
 @Module({
-  providers: [CRUDService, QueryService, DateScalar, TelfService],
-  exports: [CRUDService, QueryService, TelfService],
+  providers: [CRUDService, QueryService, DateScalar],
+  exports: [CRUDService, QueryService],
 })
 export class CommonModule {}
