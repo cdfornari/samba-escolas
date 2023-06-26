62787ee1b4e3bece3320cc45f6e59badd98d046a
Holeekey <danbrunosv@gmail.com>
2023-06-25 21:37:46 -0400

    create patroc fix #3

--- apps/api/src/patrocinios/patrocinios.service.ts
+++ apps/api/src/patrocinios/patrocinios.service.ts
@@ -23,8 +23,8 @@ export class PatrociniosService {
         await this.queryService.select<Patrocinio[]>(
           this.tableName,
           null,
-          input.id_jur? `id_escuela = ${input.id_escuela} AND fecha_fin IS NULL AND id_jur = ${input.id_jur} )`
-          : `id_escuela = ${input.id_escuela} AND fecha_fin IS NULL AND id_nat = ${input.id_nat} )`
+          //input.id_jur? `id_escuela = ${input.id_escuela} AND fecha_fin IS NULL AND id_jur = ${input.id_jur} )`
+          //: `id_escuela = ${input.id_escuela} AND fecha_fin IS NULL AND id_nat = ${input.id_nat} )`
           )
       ).length > 0
     )
